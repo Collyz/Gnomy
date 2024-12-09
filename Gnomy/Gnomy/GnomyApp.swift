c516fd56033862032ba40f42158553bc119ef20d
Collyz <m.mowla03151@gmail.com>
2024-12-09 09:06:17 -0500

    new project init

---
+++ Gnomy/Gnomy/GnomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GnomyApp.swift
+//  Gnomy
+//
+//  Created by Mohammed Mowla on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GnomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
