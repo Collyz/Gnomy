d02c53f607a28b8529006368c90d5ae9b321eb08
Collyz <m.mowla03151@gmail.com>
2024-12-20 22:38:29 -0500

    reupload old code files

---
+++ Gnomy/Gnomy/MenuView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuView.swift
+//  Gnomy
+//
+//  Created by Mohammed Mowla on 12/15/24.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct MenuView: View {
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                Text("Help him home!")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(Color.green)
+                Text("Start!")
+                    .foregroundStyle(Color("DarkGreen"))
+                    .bold()
+                    .font(.largeTitle)
+                    .padding(.horizontal, 25)
+                    .padding(.vertical, 0)
+                    .background(Color.yellow)
+                    .cornerRadius(20)
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white, lineWidth: 3))
+                    .shadow(radius: 10)
+                    .padding(.bottom, 175)
+                    .padding(.top, 220)
+                    
+            }.background(
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .scaleEffect(1.07)
+            )
+            .ignoresSafeArea()
+        }
+        }
+}
+
+#Preview {
+    MenuView()
+}
