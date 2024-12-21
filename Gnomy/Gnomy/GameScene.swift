1342ed45f28fc1d349f207b2aad86a0ae1b76557
Collyz <m.mowla03151@gmail.com>
2024-12-20 23:41:48 -0500

    player x move snaps to touch

--- Gnomy/Gnomy/GameScene.swift
+++ Gnomy/Gnomy/GameScene.swift
@@ -9,101 +9,97 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    // player
+    private let player = SKSpriteNode(imageNamed: "player")
+    override func didMove(to view: SKView) {
+        // this method is called when your game scene is ready to run
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9) // world gravity
+        let background = SKSpriteNode(imageNamed: "background")
+        background.name = "background"
+        background.zPosition = -1;
+        background.scale(to: CGSize(width: 620, height: 1400))
+        addChild(background)
+        generateCollectable()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // this method is called when the user touches the screen
+        //FROM TUTORIAL quite nice :3
+//        guard let touch = touches.first else { return }
+//        let location = touch.location(in: self)
+//        let tappedNode = nodes(at: location) //allows you to get the nodes quite easily
+//        guard let tapped = tappedNode.first else { return }
+//        
+//        if tapped.name != "background" {
+//            tapped.removeFromParent()
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                self.generateCollectable()
+//            }
+//        }
+        //END OF ----- FROM TUTORIAL quite nice :3
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        // get the players location and move to finger only x pos
+        guard let touch = touches.first else { return }
+        let loc = touch.location(in: self)
+        player.position.x = loc.x
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        // this method is called when the user stops touching the screen
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // this method is called before each frame is rendered
+        //FROM TUTORIAL quite nice :3
+//        for node in children {
+//            if node.position.y < -700 {
+//                node.removeFromParent()
+//                generateCollectable()
+//            }
+//        }
+        //END OF ----- FROM TUTORIAL quite nice :3
+        
+        // On the update, move the player to the finger position
+        //TODO: remove this update once base floor is created
+        if(player.position.y < -700) {
+            player.removeFromParent()
+            generateCollectable()
+        }
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // move on touch move
+        guard let touch = touches.first else { return }
+        let loc = touch.location(in: self)
+        player.position.x = loc.x
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        //
     }
     
+    func movePlayer(touchPos: CGPoint) {
+        //TODO: move player offset from touch (i.e. from finger)
+    }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func generateCollectable() {
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
+        player.name = "collectable"
+        player.size = CGSize(width: 90, height: 90)
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
+        // adding physics
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
         
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.affectedByGravity = true
         
-        self.lastUpdateTime = currentTime
+        player.texture!.filteringMode = .nearest;
+        player.zPosition = 0;
+        addChild(player)
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 700)
     }
 }
+
